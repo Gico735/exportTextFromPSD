b5b024a402e77c9d01d95ed9a92292a4da5b290a
Sergey Ganin <mcheguevara2@gmail.com>
2019-08-20 18:57:35 +0300

    doublicate refs

--- bin/exportRef.js
+++ bin/exportRef.js
@@ -1,83 +1,88 @@
 #!/usr/bin/env node
-const PSD = require('psd')
-const fs = require('fs')
-const path = require('path')
+const PSD = require("psd");
+const fs = require("fs");
+const path = require("path");
 
-const arrPsd = []
-const callDir = process.env.PWD
-let time = Date.now() / 1000
-let arrRefs = {}
-let haveAnyRef = false
-
-
+const arrPsd = [];
+const callDir = process.env.PWD;
+let time = Date.now() / 1000;
+let arrRefs = {};
+let haveAnyRef = false;
 
 const readDir = () => {
-  console.log("Gotta Catch 'Em All")
-  const arrDir = fs.readdirSync(callDir)
+  console.log("Gotta Catch 'Em All");
+  const arrDir = fs.readdirSync(callDir);
   arrDir.map(el => {
-    const extName = path.extname(el)
-    if (extName === '.psd') {
-      arrPsd.push(el)
+    const extName = path.extname(el);
+    if (extName === ".psd") {
+      arrPsd.push(el);
     }
-  })
+  });
   if (arrPsd.length === 0) {
-    console.warn("\x1b[41m", "I don't see psd!")
-    console.log("\x1b[0m")
-    process.exit(1)
+    console.warn("\x1b[41m", "I don't see psd!");
+    console.log("\x1b[0m");
+    process.exit(1);
   }
-  return arrPsd
-}
+  return arrPsd;
+};
 
 const chakeNameOfLay = (el, file) => {
-  if (el.type === 'group') {
+  if (el.type === "group") {
     el.children.map(child => {
-      return chakeNameOfLay(child, file)
-    })
+      return chakeNameOfLay(child, file);
+    });
   } else {
-    if (el.name.toLowerCase() === 'ref') {
-      haveAnyRef = true
-      return writeRefToFile(el, file)
+    if (el.name.toLowerCase() === "ref") {
+      haveAnyRef = true;
+      return writeRefToFile(el, file);
     }
   }
-}
-
+};
 
 const writeRefToFile = (el, file) => {
-  file = file.replace('.psd', '')
-  const text = el.text.value
-  let strRef = text.split(/[\r\u0003]/g)
-  if (text.search(/[А-Яа-я]\d\./gi) !== -1 ||
+  file = file.replace(".psd", "");
+  const text = el.text.value;
+  let strRef = text.split(/[\r\u0003]/g);
+  if (
+    text.search(/[А-Яа-я]\d\./gi) !== -1 ||
     text.search(/[А-Яа-я]\d-\d/gi) !== -1 ||
     text.search(/[А-Яа-я]\d,\d/gi) !== -1 ||
-    text.search(/[A-Za-z]\d[A-Za-z]/gi) !== -1) {
-    console.warn("\x1b[35m", `Look in ${file}.PSD maybe you find sup/sub-string in ref`)
-    console.log("\x1b[0m")
+    text.search(/[A-Za-z]\d[A-Za-z]/gi) !== -1
+  ) {
+    console.warn(
+      "\x1b[35m",
+      `Look in ${file}.PSD maybe you find sup/sub-string in ref`
+    );
+    console.log("\x1b[0m");
   }
   strRef = strRef.filter((el, i) => {
-    return !!el.trim()
-  })
-  return arrRefs[file] = strRef
-}
-
-
-readDir()
-
-arrPsd.map((file) => {
-  const psd = PSD.fromFile(`${callDir}/${file}`)
-  psd.parse()
-  const child = psd.tree().export().children
-  child.some(layers => {
-    return chakeNameOfLay(layers, file)
-  })
-  if (!haveAnyRef) {
-    console.warn("\x1b[35m", `I don't see REF layer! in ${file}`)
-    console.log("")
+    return !!el.trim();
+  });
+  if (arrRefs[file] === undefined) {
+    return (arrRefs[file] = strRef);
   } else {
-    haveAnyRef = false
+    throw `dublicate reference ${file}.psd`;
   }
-})
-const json = JSON.stringify(arrRefs)
+};
 
-fs.writeFileSync(`${callDir}/refs.json`, json)
-time = Date.now() / 1000 - time
-console.log('Время выполнения = ', time.toFixed(2))
+readDir();
+
+arrPsd.map(file => {
+  const psd = PSD.fromFile(`${callDir}/${file}`);
+  psd.parse();
+  const child = psd.tree().export().children;
+  child.some(layers => {
+    return chakeNameOfLay(layers, file);
+  });
+  if (!haveAnyRef) {
+    console.warn("\x1b[35m", `I don't see REF layer! in ${file}`);
+    console.log("");
+  } else {
+    haveAnyRef = false;
+  }
+});
+const json = JSON.stringify(arrRefs);
+
+fs.writeFileSync(`${callDir}/refs.json`, json);
+time = Date.now() / 1000 - time;
+console.log("Время выполнения = ", time.toFixed(2));
