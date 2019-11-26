203e9356b6103fad3f011e8e02e468789ee3676f
Gregory Zdanovich <g.zdanovich@onpoint.ru>
2019-11-26 18:15:04 +0300

    fix

--- bin/exportRef.js
+++ bin/exportRef.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
 const main = require('./utils/utils')
 
+const checkSubStr = (text, file) => {
+  if (
+    text.search(/[А-Яа-я]\d\./gi) !== -1 ||
+    text.search(/[А-Яа-я]\d-\d/gi) !== -1 ||
+    text.search(/[А-Яа-я]\d,\d/gi) !== -1 ||
+    text.search(/[A-Za-z]\d[A-Za-z]/gi) !== -1
+  ) {
+    console.warn('\x1b[35m', `Look in ${file}.psd maybe you find sup/sub-string in ref`)
+    console.log('\x1b[0m')
+  }
+}
+
 const checkValidLayer = (layer, file) => {
   //if have layer name="noref", it's mean noref in PSD
   if (layer.name.toLowerCase() === 'noref') return false
@@ -12,17 +24,9 @@ const checkValidLayer = (layer, file) => {
 
 const writeRefToFile = (layer, file) => {
   const text = layer.text.value
+  checkSubStr(text, file)
   let strRef = text.replace(/\u0003/g, '</br>')
   strRef = strRef.split(/\r/g)
-  if (
-    text.search(/[А-Яа-я]\d\./gi) !== -1 ||
-    text.search(/[А-Яа-я]\d-\d/gi) !== -1 ||
-    text.search(/[А-Яа-я]\d,\d/gi) !== -1 ||
-    text.search(/[A-Za-z]\d[A-Za-z]/gi) !== -1
-  ) {
-    console.warn('\x1b[35m', `Look in ${file}.psd maybe you find sup/sub-string in ref`)
-    console.log('\x1b[0m')
-  }
   strRef = strRef.filter(el => !!el.trim())
   return strRef
 }
